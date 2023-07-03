00d3a277e3d886e50b4bab4ac0f457d8f56e2e54
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-03 13:50:34 +0400

    Task2 Age

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ExceptionsTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### Task 1 Dividing ####");
+            Console.WriteLine();
+            Console.WriteLine("Einter the first number : ");
+            int FirstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Einter the second number : ");
+            int SecondNumber = int.Parse(Console.ReadLine());
+           
+            try
+            {
+                dividing(FirstNumber, SecondNumber);
+            } catch (DivideByZeroException ex) 
+            { 
+                Console.WriteLine("Divide by zero exception");
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("### Task 2 Checking age ####");
+            Console.WriteLine();
+
+            age(12);
+
+        }
+
+
+        public static void dividing(decimal a , decimal b)
+        {
+            decimal c = a / b;
+            //float c = Convert.float(Console.ReadLine());
+            Console.WriteLine($" {a}/{b} = {c} ");
+        }
+
+        public static void age( int a) 
+        {
+            Console.WriteLine("Einter Youre Age : ");
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (age <= 0)
+                {
+                    Console.WriteLine("Invalid Age ");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Invalid input exception");
+            }
+        }
+    }
+}
